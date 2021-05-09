7ba2daf41b1d45b06a18a8a588e3359426eb9825
Rafael Ricardo Vieceli <rafaelricardo3@gmail.com>
2021-05-09 19:16:49 +0200

    Controllers implementation

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,13 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = request.headers.user_id as string;
+    try {
+      const all = this.listAllUsersUseCase.execute({ user_id });
+      return response.json(all);
+    } catch (e) {
+      return response.status(400).json({ error: e.message });
+    }
   }
 }
 
